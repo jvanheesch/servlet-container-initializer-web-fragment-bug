c1e96add24e4731cd517330eec9b5a5088f84a22
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-11-23 21:22:33 +0100

    Added another-external-jar, and all is as expected: all thee ServletContainerInitializers are called.

---
+++ another-external-jar/src/main/java/com/github/jvanheesch/anotherexternal/AnotherExternalServletContainerInitializer.java
@@ -0,0 +1,13 @@
+package com.github.jvanheesch.anotherexternal;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class AnotherExternalServletContainerInitializer implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("AnotherExternalServletContainerInitializer.onStartup");
+    }
+}
